d2a803c5cad5893f7740812ef72b319e8153f516
ettiev <126219891+ettiev@users.noreply.github.com>
2024-09-10 20:19:26 +0200

    Manage Expense modified
    - Navigation to Manage Expense - edit mode and add mode
    - Styling for Manage Expense
    - New Custom Icon Button
    - Add and delete icon buttons added

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
 import { GlobalStyles } from './constants/styles';
+import IconButton from './components/UI/IconButton';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -21,12 +22,22 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
   return (
     <BottomTabs.Navigator
-      screenOptions={{
+      screenOptions={ ({ navigation }) => ({
         headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         headerTintColor: 'white',
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-        tabBarActiveTintColor: GlobalStyles.colors.accent500
-      }}
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({tintColor}) => (
+          <IconButton 
+            icon="add" 
+            size={24} 
+            color={tintColor} 
+            onPress={ () => {
+              navigation.navigate('ManageExpense')
+            }} 
+          />
+        )
+      })}
     >
       <BottomTabs.Screen 
         name='RecentExpenses' 
@@ -55,7 +66,12 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: 'white'
+          }}
+        >
           <Stack.Screen 
             name='ExpensesOverview' 
             component={ ExpensesOverview }
@@ -63,7 +79,13 @@ export default function App() {
               headerShown: false
             }}
           />
-          <Stack.Screen name='ManageExpense' component={ ManageExpense }/>
+          <Stack.Screen 
+            name='ManageExpense' 
+            component={ ManageExpense }
+            options={{
+              presentation: 'modal'  //optional in Android
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
