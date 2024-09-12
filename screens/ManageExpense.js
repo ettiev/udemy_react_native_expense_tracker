0b2b4bb90da45f4dbce606cdf8423e9fde564085
ettiev <126219891+ettiev@users.noreply.github.com>
2024-09-12 19:27:53 +0200

    Expense Form and Form Submission
    Added Expense Form
    Added custom Input component
    Added form validation
    Fixed error in date utilities

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -5,11 +5,15 @@ import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
     const expensesCtx = useContext(ExpensesContext);
     
     const editedExpenseId = route.params?.expenseId;
+    const selectedExpense = expensesCtx.expenses.find(
+        (expense) => expense.id === editedExpenseId
+    );
     const isEditing = !!editedExpenseId;
 
     useLayoutEffect(() => {
@@ -27,29 +31,23 @@ function ManageExpense({ route, navigation }) {
         navigation.goBack();
     }
 
-    function confirmHandler() {
+    function confirmHandler(expenseData) {
         if (isEditing) {
-            expensesCtx.updateExpense(editedExpenseId, {
-                description: 'Test Update',
-                amount: 19.99,
-                date: new Date('2024-09-08')
-            })
+            expensesCtx.updateExpense(editedExpenseId, expenseData)
         } else {
-            expensesCtx.addExpense({
-                description: 'Test Add',
-                amount: 29.99,
-                date: new Date('2024-09-10')
-            })
+            expensesCtx.addExpense(expenseData)
         }
         navigation.goBack();
     }
 
     return (
         <View style={ styles.container }>
-            <View style={styles.buttons}>
-                <Button style={styles.button} mode='flat' onPress={cancelHandler}>Cancel</Button>
-                <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
-            </View>
+            <ExpenseForm 
+                onCancel={cancelHandler}
+                onSubmit={confirmHandler}
+                submitButtonLabel={isEditing ? 'Update' : 'Add'}
+                defaultValues={selectedExpense}
+            />
             {isEditing && (
                 <View style={ styles.deleteContainer }>
                     <IconButton 
@@ -72,20 +70,11 @@ const styles = StyleSheet.create({
         padding: 24,
         backgroundColor: GlobalStyles.colors.primary800
     },
-    buttons: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
     deleteContainer: {
         marginTop: 16,
         paddingTop: 8,
         borderTopWidth: 2,
         borderTopColor: GlobalStyles.colors.primary200,
         alignItems: 'center'
-    },
-    button: {
-        minWidth: 120,
-        marginHorizontal: 8 
     }
 })
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { Text } from "react-native";
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from "../store/expenses-context";
@@ -6,11 +7,10 @@ import { getDateMinusDays } from "../util/date";
 
 function RecentExpenses() {
     const expensesCtx = useContext(ExpensesContext);
-    
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
     const recentExpenses = expensesCtx.expenses.filter((expense) => {
-        const today = new Date();
-        const date7DaysAgo = getDateMinusDays(today, 7);
-        return (expense.date >= date7DaysAgo) && (expense.date <= today)
+        return (expense.date >= date7DaysAgo && expense.date <= today)
     })
     
     return (
